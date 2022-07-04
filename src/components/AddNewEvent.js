8e57e39f099540cf4c9ef518af1668667695f369
Arun Kumar <arunkumar413@gmail.com>
2022-07-04 09:38:54 +0000

    saving changes

--- src/components/AddNewEvent.js
+++ src/components/AddNewEvent.js
@@ -1,25 +1,86 @@
 import react, { useState, useEffect } from "react";
 
 export function AddNewEvent(props) {
+  const [event, setEvent] = useState({
+    title: "An event to remember",
+    description: "description of the event",
+    location: "New York",
+    link: "http://test.com",
+    guestsInvited: [],
+    guestsAttending: [],
+  });
   const [isEditModeOn, setEditMode] = useState(false);
+  const [guestInput, setGuestInput] = useState("");
 
   function toggleEditMode() {
     setEditMode(isEditModeOn ? false : true);
+  }
+
+  function handleFormInput(evt) {}
+  function handleGuestChange(evt) {
+    setGuestInput(evt.target.value);
+  }
+
+  function addNewGuest() {
+    setEvent(function (prevState) {
+      return {
+        ...prevState,
+        guestsInvited: [...prevState.guestsInvited, guestInput],
+      };
+    });
+    setGuestInput("");
+  }
+
+  useEffect(function () {}, []);
+
+  function handleKeyDown(evt) {
+    if (evt.key === "Enter") {
+      setEvent(function (prevState) {
+        return {
+          ...prevState,
+          guestsInvited: [...prevState.guestsInvited, guestInput],
+        };
+      });
+      setGuestInput("");
+    }
   }
 
   const EditModeContent = (
     <div className="content-items">
       <div className="content-item">
         <span class="material-symbols-outlined">title</span>{" "}
-        <input type="date-time-local" />{" "}
+        <input name="title" onChange={handleFormInput} type="text" />{" "}
       </div>
       <div className="content-item">
         <span class="material-symbols-outlined">event</span>{" "}
-        <input type="date-time-local" />
+        <input
+          name="date"
+          onChange={handleFormInput}
+          className="edit-date"
+          type="datetime-local"
+        />
+        <span className="form-input"> date </span>
       </div>
       <div className="content-item">
         <span class="material-symbols-outlined">pin_drop</span>{" "}
-        <input type="text" />
+        <input onChange={handleFormInput} name="location" type="text" />
+      </div>
+      <div className="content-item">
+        <span class="material-symbols-outlined">link</span>{" "}
+        <input onChange={handleFormInput} name="link" type="text" />
+      </div>
+      <div className="content-item">
+        <span class="material-symbols-outlined">person_add</span>{" "}
+        <input
+          value={guestInput}
+          onKeyDown={handleKeyDown}
+          onChange={handleGuestChange}
+          name="addGuest"
+          type="email"
+        />
+        <span onClick={addNewGuest} class="material-symbols-outlined">
+          add
+        </span>{" "}
       </div>
     </div>
   );
@@ -37,6 +98,10 @@ export function AddNewEvent(props) {
       <div className="content-item">
         <span class="material-symbols-outlined">pin_drop</span>{" "}
         <span className="form-input"> Location </span>
+      </div>
+      <div className="content-item">
+        <span class="material-symbols-outlined">link</span>{" "}
+        <span className="form-input"> Link </span>
       </div>
     </div>
   );
